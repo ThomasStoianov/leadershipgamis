190aad28a13718b15b29552c53f733067fb483f6
Thomas-Stoianov <thomasgstoianov2@gmail.com>
2024-05-20 17:24:33 -0300

    feat: Menu Hamburguer pronto

--- script.js
+++ script.js
@@ -20,3 +20,55 @@ function AbrirFechar() {
         iconebarras.style.display = "inline"
     }
 }
+
+
+window.onresize = () => {
+    menu.classList.remove("menu-fechado");
+    iconeX.style.display = "inline"
+    iconebarras.style.display = "none"
+}
+
+
+
+const SolicitarOrcamento = (event) => {
+
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value
+    let valorTelefone = document.getElementById("campo-telefone").value
+
+    console.log(valorNome);
+    console.log(valorEmail);
+    console.log(valorTelefone);
+
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        telefone: valorTelefone,
+    }
+
+    fetch("http://localhost:3000/solicitacoes" , {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+    })
+
+    .then(resposta => {
+        console.log(resposta)
+
+        document.querySelector("#contato form").reset()
+
+        alert("Solicitação cadastrada")
+    })
+
+    .catch(erro => {
+        console.error(erro)
+        alert("Erro na descrição")
+    
+    })
+
+    event.preventDefault()
+
+
+}
